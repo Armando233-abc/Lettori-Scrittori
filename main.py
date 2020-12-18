3dd3ab1738ac6a11c1c0d864ba10fb534515d449
Armando233-abc <armandovigliotti5@gmail.com>
2020-12-18 17:26:33 +0100

    codice

---
+++ main.py
@@ -0,0 +1,40 @@
+import threading
+
+
+lettori = 0
+file = ""
+mutex = threading.Semaphore(1)
+scrivi = threading.Semaphore(1)
+
+# funzioni 
+def lettore():
+    global lettori, file
+    mutex.acquire()
+    lettori += 1
+    if lettori == 1:
+        scrivi.acquire()
+    mutex.release()
+    file = open("lettura.txt", "r").read()
+    print("il lettore ha letto")
+    mutex.acquire()
+    lettori -= 1
+    if lettori == 0:
+        scrivi.release()
+
+    mutex.release()
+    
+
+
+def scrittore():
+    scrivi.acquire()
+    file = open("combinazioni.txt", "a")
+    file.write("lo scrittore ha scritto\n")
+    file.close()
+    scrivi.release()
+
+
+# main
+lettore()
+lettore()
+
+scrittore()
